f036ffb8d0bf42e02aa23ec7a40ef67c8d0201d7
Deeps <deepikkaa20@gmail.com>
2024-08-06 17:42:40 +0530

    changed commited

---
+++ Boys.java
@@ -0,0 +1,49 @@
+////import java.lang.*;
+//
+//class Human
+//{
+//	int weight=50; //instance variable
+//	int amount=6000;
+//        static String color ="pink";   // class variable
+//	
+//	private void eat()
+//	{
+//		System.out.println("Eating ...");
+//	}
+//
+//	public String walk()
+//	{
+//		return "Walking 5km daily...";
+//	}	
+//
+//}
+//
+//
+//class Boys
+//{
+//
+//	public static void main(String[] args)
+//	{
+//		int height=200;
+//	
+//		Human ravi= new Human();
+//		
+//	   	System.out.println("Welcome All");
+//		//System.out.println("Your Height : "+ height );
+//		System.out.println("Your Weight : "+ ravi.weight );		
+//		//System.out.println("Your Color: "+ color);
+//		ravi.eat();
+//		String str=ravi.walk();
+//		System.out.println(str);
+//
+//		System.out.println(ravi.walk());
+//		
+//	}
+//
+//}
+//
+//
+//
+//
+//
+//
